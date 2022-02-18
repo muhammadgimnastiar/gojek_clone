c03567422904bf87bc2e930737326f1fb5c524e3
Muhmmad Gimnastiar <adagimnas@gmail.com>
2022-02-18 21:44:51 +0800

    add image asset like banner, and icon

--- lib/widget/homepage.dart
+++ lib/widget/homepage.dart
@@ -11,11 +11,113 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        SearchBar(),
-        CardGopay(),
-      ],
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        children: const [
+          SearchBar(),
+          CardGopay(),
+          IconList(),
+          TitleSection(),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            "lib/assets/icon/gopaylater.png",
+            height: 14,
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            "Lebih Hemat Pakai GoPayLater",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 4),
+          const Text(
+              "Yuk, belanja di tokopedia pakai gopaylater. Untung belkan juta ",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
+          const SizedBox(height: 15),
+          const CardBanner(
+            image: "lib/assets/banner/promo.png",
+            text: "Aktifkan dan sambungkan gopay& gopaylater ke tokopedia",
+          ),
+          const CardBanner(
+            image: "lib/assets/banner/tokped.png",
+            text: "Aktifkan dan sambungkan gopay& gopaylater ke tokopedia",
+          ),
+          const CardBanner(
+            image: "lib/assets/banner/tokped2.png",
+            text: "Aktifkan dan sambungkan gopay& gopaylater ke tokopedia",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CardBanner extends StatelessWidget {
+  const CardBanner({Key? key, this.text = "", this.image = ""})
+      : super(key: key);
+
+  final String image;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Card(
+        elevation: 2,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: InkWell(
+          onTap: () {},
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                alignment: Alignment.bottomLeft,
+                children: [
+                  Ink.image(
+                    height: 170,
+                    image: AssetImage(image),
+                    fit: BoxFit.fitWidth,
+                  ),
+                ],
+              ),
+              const Padding(
+                padding:
+                    EdgeInsets.only(left: 20, right: 20.0, top: 20, bottom: 10),
+                child: Text(
+                  "Makin Seru",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 20.0, right: 20, bottom: 20),
+                child: Text(
+                  text,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w400, fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -88,7 +190,8 @@ class CardGopay extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-                padding: EdgeInsets.only(left: 12),
+                padding: const EdgeInsets.only(left: 12),
+                margin: const EdgeInsets.only(right: 8),
                 height: 78,
                 width: 140,
                 decoration: BoxDecoration(
@@ -96,25 +199,26 @@ class CardGopay extends StatelessWidget {
                     borderRadius: BorderRadius.circular(8)),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    IconWithLabel(
-                      "Gopay",
-                      icon: Icons.wallet_giftcard_rounded,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Image.asset("lib/assets/icon/gopay.png",
+                          width: 57, height: 14),
                     ),
-                    SizedBox(
-                      height: 10,
+                    const SizedBox(
+                      height: 8,
                     ),
-                    Text(
+                    const Text(
                       "Rp500.099",
                       style: TextStyle(
                           fontSize: 16,
                           color: Colors.black87,
                           fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 4,
                     ),
-                    Text(
+                    const Text(
                       "Tap for History",
                       style: TextStyle(
                           fontSize: 14,
@@ -123,17 +227,24 @@ class CardGopay extends StatelessWidget {
                     )
                   ],
                 )),
-            const itemCardGopay(
-              "Pay",
-              icon: Icons.payment_rounded,
-            ),
-            const itemCardGopay(
-              "Top Up",
-              icon: Icons.add_box_rounded,
-            ),
-            const itemCardGopay(
-              "Explore",
-              icon: Icons.explore_rounded,
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Row(
+                children: const [
+                  IconMenu(
+                    "Pay",
+                    icon: "lib/assets/icon/pay.png",
+                  ),
+                  IconMenu(
+                    "Top Up",
+                    icon: "lib/assets/icon/topup.png",
+                  ),
+                  IconMenu(
+                    "Explore",
+                    icon: "lib/assets/icon/explore.png",
+                  ),
+                ],
+              ),
             )
           ],
         ),
@@ -142,64 +253,103 @@ class CardGopay extends StatelessWidget {
   }
 }
 
-class itemCardGopay extends StatelessWidget {
-  const itemCardGopay(this.text, {Key? key, this.icon = Icons.abc})
+class IconMenu extends StatelessWidget {
+  const IconMenu(this.text,
+      {Key? key, this.icon = "", this.color = Colors.white, this.size = 24})
       : super(key: key);
 
-  final IconData icon;
+  final Color color;
+  final String icon;
   final String text;
+  final double size;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 0),
-      child: Column(
-        children: [
-          Icon(
-            icon,
-            size: 32,
-            color: Colors.white,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 0),
+          child: Column(
+            children: [
+              Image.asset(
+                icon,
+                width: size,
+                height: size,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                text,
+                style: TextStyle(
+                    color: color, fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
           ),
-          SizedBox(height: 5),
-          Text(
-            text,
-            style: TextStyle(
-                color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
-          )
-        ],
+        ),
       ),
     );
   }
 }
 
-class IconWithLabel extends StatelessWidget {
-  const IconWithLabel(this.text,
-      {Key? key, this.icon = Icons.abc, this.iconSize = 16})
-      : super(key: key);
-
-  final IconData icon;
-  final String text;
-  final double iconSize;
-
+class IconList extends StatelessWidget {
+  const IconList({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0, 6.0, 8.0, 0),
-      child: Row(
-        children: [
-          Icon(
-            icon,
-            size: iconSize,
-            color: AppColors.blue,
-          ),
-          SizedBox(width: 5),
-          Text(
-            text,
-            style: TextStyle(
-                color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
-          )
-        ],
-      ),
+    return GridView.count(
+      physics: const BouncingScrollPhysics(),
+      shrinkWrap: true,
+      crossAxisCount: 4,
+      children: const [
+        IconMenu(
+          "GoRide",
+          icon: "lib/assets/icon/goride.png",
+          color: Colors.black87,
+          size: 40,
+        ),
+        IconMenu(
+          "GoCar",
+          icon: "lib/assets/icon/gocar.png",
+          color: Colors.black87,
+          size: 40,
+        ),
+        IconMenu(
+          "GoFood",
+          icon: "lib/assets/icon/gofood.png",
+          color: Colors.black87,
+          size: 40,
+        ),
+        IconMenu(
+          "GoRide",
+          icon: "lib/assets/icon/gosend.png",
+          color: Colors.black87,
+          size: 40,
+        ),
+        IconMenu(
+          "GoMart",
+          icon: "lib/assets/icon/gomart.png",
+          color: Colors.black87,
+          size: 40,
+        ),
+        IconMenu(
+          "GoPulsa",
+          icon: "lib/assets/icon/gopulsa.png",
+          color: Colors.black87,
+          size: 40,
+        ),
+        IconMenu(
+          "GoClub",
+          icon: "lib/assets/icon/goclub.png",
+          color: Colors.black87,
+          size: 40,
+        ),
+        IconMenu(
+          "GoRide",
+          icon: "lib/assets/icon/other.png",
+          color: Colors.black87,
+          size: 40,
+        ),
+      ],
     );
   }
 }
